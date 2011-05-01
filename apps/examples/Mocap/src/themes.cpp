596cb3b94fafec132430e52991b38d3d74feb262
Paige Ponzeka <paigepon@gmail.com>
2011-05-01 00:57:26 -0400

    added Themes and support for generic theme, currently only supporting colors, sound to come

---
+++ apps/examples/Mocap/src/themes.cpp
@@ -0,0 +1,9 @@
+//
+//  theme.cpp
+//  jestureCap
+//
+//  Created by Paige Ponzeka on 5/1/11.
+//  Copyright 2011 NYU. All rights reserved.
+//
+
+#include "theme.h"
---
+++ apps/examples/Mocap/src/themes.h
@@ -0,0 +1,7 @@
+//
+//  theme.h
+//  jestureCap
+//
+//  Created by Paige Ponzeka on 5/1/11.
+//  Copyright 2011 NYU. All rights reserved.
+//
